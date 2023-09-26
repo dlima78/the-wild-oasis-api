b7b8b15998f825ae7abc3095366f5be59ba11245
Eduardo Lima <dlima78@hotmail.com>
2023-09-26 16:03:50 -0300

    feat: ensure AddCabinController implements Controll interface and returns 204 on success

--- src/presentation/controllers/add-cabin-controller.ts
+++ src/presentation/controllers/add-cabin-controller.ts
@@ -1,9 +1,24 @@
 import { type AddCabin } from '@/domain/usecases/add-cabin'
+import { type Controller, type HttpResponse } from '@/presentation/protocols'
 
-export class AddCabinController {
+export class AddCabinController implements Controller {
   constructor (private readonly addCabin: AddCabin) {}
-  async handle (request: any): Promise<any> {
+  async handle (request: AddCabinController.Request): Promise<HttpResponse> {
     await this.addCabin.add(request)
-    return {}
+    return {
+      statusCode: 204,
+      body: null
+    }
+  }
+}
+
+export namespace AddCabinController {
+  export type Request = {
+    name: string
+    maxCapacity: number
+    regularPrice: number
+    discount: number
+    description: string
+    image?: string | undefined
   }
 }

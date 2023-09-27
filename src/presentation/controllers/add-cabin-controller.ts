605b8b7dc13a81f9426fbe4568051bff889e2540
Eduardo Lima <dlima78@hotmail.com>
2023-09-27 20:59:42 -0300

    feat: ensure AddCabinController returns 400 if Validation fails

--- src/presentation/controllers/add-cabin-controller.ts
+++ src/presentation/controllers/add-cabin-controller.ts
@@ -1,11 +1,14 @@
 import { type AddCabin } from '@/domain/usecases/add-cabin'
-import { noContent } from '@/presentation/helpers'
+import { badRequest, noContent } from '@/presentation/helpers'
 import { type Controller, type HttpResponse, type Validation } from '@/presentation/protocols'
 
 export class AddCabinController implements Controller {
   constructor (private readonly addCabin: AddCabin, private readonly validation: Validation) {}
   async handle (request: AddCabinController.Request): Promise<HttpResponse> {
-    this.validation.validate(request)
+    const error = this.validation.validate(request)
+    if (error != null) {
+      return badRequest(error)
+    }
     await this.addCabin.add(request)
 
     return noContent()

0ac1ce38f08f3c12f21a5a74b00691cf377ce264
Eduardo Lima <dlima78@hotmail.com>
2023-10-03 10:12:26 -0300

    chore: configure express server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,6 @@
+import 'module-alias/register'
 import app from './config/app'
 const port = 5050
-app.listen(port, () => { console.log(`Server running at http://localhost:${port}`) })
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})

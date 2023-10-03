4261b4ab1dfe7825dc8da85368c35addbf9cae40
Eduardo Lima <dlima78@hotmail.com>
2023-10-03 11:39:09 -0300

    refactor: connect to mongodb before integration tests

--- tests/main/routes/cabin-routes.test.ts
+++ tests/main/routes/cabin-routes.test.ts
@@ -1,8 +1,24 @@
-import request from 'supertest'
-
+import { MongoHelper } from '@/infra/db'
 import app from '@/main/config/app'
 
+import { type Collection } from 'mongodb'
+import request from 'supertest'
+
+let cabinCollection: Collection
+
 describe('Cabin', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    cabinCollection = MongoHelper.getCollection('cabins')
+    await cabinCollection.deleteMany({})
+  })
   test('should ', async () => {
     await request(app)
       .post('/api/cabin')

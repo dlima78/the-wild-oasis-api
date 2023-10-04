ca1f28d009c68839fbf066a32e74dc84377cb12e
Eduardo Lima <dlima78@hotmail.com>
2023-10-04 14:29:24 -0300

    test: ensure LogControllerDecorator calls Controller handle

---
+++ tests/main/decorator/log-controller-decorator.spec.ts
@@ -0,0 +1,36 @@
+import { type Controller, type HttpResponse } from '@/presentation/protocols'
+import { noContent } from '@/presentation/helpers'
+import { LogControllerDecorator } from '@/main/decorator'
+import { faker } from '@faker-js/faker'
+
+class ControllerSpy implements Controller {
+  httpResponse = noContent()
+  request: any
+  async handle (request: any): Promise<HttpResponse> {
+    this.request = request
+    return this.httpResponse
+  }
+}
+
+type SutTypes = {
+  sut: LogControllerDecorator
+  controllerSpy: ControllerSpy
+}
+
+const makeSut = (): SutTypes => {
+  const controllerSpy = new ControllerSpy()
+  const sut = new LogControllerDecorator(controllerSpy)
+  return {
+    controllerSpy,
+    sut
+  }
+}
+
+describe('LogController Decorator', () => {
+  test('should call controller handle', async () => {
+    const { sut, controllerSpy } = makeSut()
+    const request = faker.lorem.sentence()
+    await sut.handle(request)
+    expect(controllerSpy.request).toEqual(request)
+  })
+})

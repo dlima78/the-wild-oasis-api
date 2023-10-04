1a83e376a3fc34a2069ff22f95414b599591ced0
Eduardo Lima <dlima78@hotmail.com>
2023-10-04 18:28:39 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository with correct error if the controller returns a server error

--- tests/main/decorator/log-controller-decorator.spec.ts
+++ tests/main/decorator/log-controller-decorator.spec.ts
@@ -1,7 +1,8 @@
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { noContent } from '@/presentation/helpers'
+import { noContent, serverError } from '@/presentation/helpers'
 import { LogControllerDecorator } from '@/main/decorator'
 import { faker } from '@faker-js/faker'
+import { LogErrorRepositorySpy } from '@/tests/data/mock'
 
 class ControllerSpy implements Controller {
   httpResponse = noContent()
@@ -12,16 +13,25 @@ class ControllerSpy implements Controller {
   }
 }
 
+const mockServerError = (): HttpResponse => {
+  const fakeError = new Error()
+  fakeError.stack = 'any_stack'
+  return serverError(fakeError)
+}
+
 type SutTypes = {
   sut: LogControllerDecorator
   controllerSpy: ControllerSpy
+  logErrorRepositorySpy: LogErrorRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const controllerSpy = new ControllerSpy()
-  const sut = new LogControllerDecorator(controllerSpy)
+  const logErrorRepositorySpy = new LogErrorRepositorySpy()
+  const sut = new LogControllerDecorator(controllerSpy, logErrorRepositorySpy)
   return {
     controllerSpy,
+    logErrorRepositorySpy,
     sut
   }
 }
@@ -38,5 +48,13 @@ describe('LogController Decorator', () => {
     const { sut, controllerSpy } = makeSut()
     const httpResponse = await sut.handle(faker.lorem.sentence())
     expect(httpResponse).toEqual(controllerSpy.httpResponse)
+  })
+
+  test('should call LogErrorRepository with correct error if controller returns a server error', async () => {
+    const { sut, controllerSpy, logErrorRepositorySpy } = makeSut()
+    const serverError = mockServerError()
+    controllerSpy.httpResponse = serverError
+    await sut.handle(faker.lorem.sentence())
+    expect(logErrorRepositorySpy.stack).toBe(serverError.body.stack)
   })
 })

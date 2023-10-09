0fa1249ef8b00bdeb4049881e4b0e72194bf151d
Eduardo Lima <dlima78@hotmail.com>
2023-10-09 19:04:39 -0300

    test: ensure AccountMongoRespository add account on success

---
+++ tests/infra/db/mongodb/account-mongo-repository.spec.ts
@@ -0,0 +1,32 @@
+import { AccountMongoRepository, MongoHelper } from '@/infra/db'
+import { mockAddAccountParams } from '@/tests/domain/mocks'
+import { type Collection } from 'mongodb'
+
+let accountCollection: Collection
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
+describe('AccountMongoReporitory', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+  describe('add()', () => {
+    test('should add account on success', async () => {
+      const sut = makeSut()
+      const addAccounts = mockAddAccountParams()
+      const isValid = await sut.add(addAccounts)
+      expect(isValid).toBe(true)
+    })
+  })
+})

de21e63ce826928fcebfa0fffa76f0a4ca394b43
Eduardo Lima <dlima78@hotmail.com>
2023-10-10 16:21:07 -0300

    test: ensure EmailValidation calls EmailValidtor with correct email

--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -27,4 +27,11 @@ describe('Email Validation', () => {
     const error = sut.validate({ [field]: email })
     expect(error).toEqual(new InvalidParamError(field))
   })
+
+  test('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const email = faker.internet.email()
+    sut.validate({ [field]: email })
+    expect(emailValidatorSpy.email).toBe(email)
+  })
 })

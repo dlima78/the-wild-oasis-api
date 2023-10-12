48949b1dbf156872b1fcd28df88b97378fa24a7a
Eduardo Lima <dlima78@hotmail.com>
2023-10-12 14:33:56 -0300

    feat: ensure DbAuthentication calss Encrypter with correct plaintext

--- src/data/usecases/db-authentication.ts
+++ src/data/usecases/db-authentication.ts
@@ -1,9 +1,13 @@
 import { type Authentication } from '@/domain/usecases'
-import { type LoadAccoutByEmailRepository } from '@/data/protocols'
+import {
+  type Encrypter,
+  type LoadAccoutByEmailRepository
+} from '@/data/protocols'
 
 export class DbAuthentication implements Authentication {
   constructor (
-    private readonly loadAccountByEmailRepository: LoadAccoutByEmailRepository
+    private readonly loadAccountByEmailRepository: LoadAccoutByEmailRepository,
+    private readonly encrypter: Encrypter
   ) {}
 
   async auth (
@@ -11,12 +15,9 @@ export class DbAuthentication implements Authentication {
   ): Promise<Authentication.Result> {
     const { email } = authenticationParams
     const account = await this.loadAccountByEmailRepository.loadByEmail(email)
-    if (!account) {
-      return null
+    if (account) {
+      await this.encrypter.encrypt(account.id)
     }
-    return {
-      accessToken: '',
-      name: ''
-    }
+    return null
   }
 }

bfcfb28ba4de29d0dde8b2e4b76fa6715707c64d
Eduardo Lima <dlima78@hotmail.com>
2023-10-13 11:31:37 -0300

    feat: ensure AccountMongoRepository returns true if checkByEmail is valid

--- src/infra/db/mongodb/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-mongo-repository.ts
@@ -1,10 +1,20 @@
-import { type AddAccountRepository } from '@/data/protocols'
+import {
+  type CheckAccountByEmailRepository,
+  type AddAccountRepository
+} from '@/data/protocols'
 import { MongoHelper } from '@/infra/db'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+implements AddAccountRepository, CheckAccountByEmailRepository {
   async add (data: AddAccountRepository.Params): Promise<boolean> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(data)
     return result !== null
+  }
+
+  async checkByEmail (email: string): Promise<CheckAccountByEmailRepository.Result> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    return account !== null
   }
 }

8d7a1bcb1a59359c2ac961086f2b79c98674132b
Eduardo Lima <dlima78@hotmail.com>
2023-10-16 14:44:28 -0300

    feat: ensure CompareFieldsValidaton return an InvalidParamError if validation fails

---
+++ src/validation/validators/compare-fields-validaton.ts
@@ -0,0 +1,16 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { type Validation } from '@/presentation/protocols'
+
+export class CompareFieldsValidation implements Validation {
+  constructor (
+    private readonly field: string,
+    private readonly fieldToCompare: string
+  ) {}
+
+  validate (input: any): Error | null {
+    if (input[this.field] !== input[this.fieldToCompare]) {
+      return new InvalidParamError(this.fieldToCompare)
+    }
+    return null
+  }
+}

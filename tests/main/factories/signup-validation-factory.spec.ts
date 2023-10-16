396250417126f38ecd92e52bf98a80d6faf86970
Eduardo Lima <dlima78@hotmail.com>
2023-10-16 15:13:21 -0300

    test: ensure SignupValidation factory calls Validation composite with all validations

---
+++ tests/main/factories/signup-validation-factory.spec.ts
@@ -0,0 +1,26 @@
+import { makeSignupValidation } from '@/main/factories'
+import {
+  ValidationComposite,
+  RequiredFieldValidation,
+  CompareFieldsValidation,
+  EmailValidation
+} from '@/validation/validators'
+import { type Validation } from '@/presentation/protocols'
+import { EmailValidatorAdapter } from '@/infra/validators'
+
+jest.mock('@/validation/validators/validation-composite')
+
+describe('SignUpValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeSignupValidation()
+    const validations: Validation[] = []
+    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    validations.push(
+      new CompareFieldsValidation('password', 'passwordConfirmation')
+    )
+    validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})

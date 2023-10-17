07a4da297f841b850c36bb2425db0e3e335267a4
Eduardo Lima <dlima78@hotmail.com>
2023-10-17 13:26:36 -0300

    feat: ensure LoginController returns 400 if validation returns an error

--- src/presentation/controllers/login-controller.ts
+++ src/presentation/controllers/login-controller.ts
@@ -1,9 +1,13 @@
 import { type Validation } from '@/presentation/protocols'
+import { badRequest } from '@/presentation/helpers'
 
 export class LoginController {
   constructor (private readonly validation: Validation) {}
   async handle (request: LoginController.Request): Promise<any> {
-    this.validation.validate(request)
+    const error = this.validation.validate(request)
+    if (error) {
+      return badRequest(error)
+    }
   }
 }
 

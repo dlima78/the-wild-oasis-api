e548a86254eca5dcfbb086ab25a40249c203c097
Eduardo Lima <dlima78@hotmail.com>
2023-10-19 10:07:21 -0300

    feat: ensure AccountMongoRepository returns an account on loadByToken without role

--- src/infra/db/mongodb/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-mongo-repository.ts
@@ -4,6 +4,7 @@ import {
   type UpdateAccessTokenRepository,
   type LoadAccoutByEmailRepository
 } from '@/data/protocols'
+import { type LoadAccountByTokenRepository } from '@/data/protocols/db/account/load-account-by-token-repository'
 import { MongoHelper } from '@/infra/db'
 import { ObjectId } from 'mongodb'
 
@@ -12,7 +13,8 @@ implements
     AddAccountRepository,
     CheckAccountByEmailRepository,
     UpdateAccessTokenRepository,
-    LoadAccoutByEmailRepository {
+    LoadAccoutByEmailRepository,
+    LoadAccountByTokenRepository {
   async add (data: AddAccountRepository.Params): Promise<boolean> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(data)
@@ -54,5 +56,14 @@ implements
         }
       }
     )
+  }
+
+  async loadByToken (
+    accessToken: string,
+    role?: string | undefined
+  ): Promise<LoadAccountByTokenRepository.Result> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ accessToken })
+    return MongoHelper.map(account)
   }
 }

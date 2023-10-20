58d4e9dbb36737e4b3d0a5dae142a750ee7549c4
Eduardo Lima <dlima78@hotmail.com>
2023-10-20 14:29:30 -0300

    test: ensure add cabin route returns 204 on add cabin with valid accessToken

--- tests/main/routes/cabin-routes.test.ts
+++ tests/main/routes/cabin-routes.test.ts
@@ -1,10 +1,35 @@
 import { MongoHelper } from '@/infra/db'
 import app from '@/main/config/app'
+import env from '@/main/config/env'
 
 import { type Collection } from 'mongodb'
 import request from 'supertest'
+import jwt from 'jsonwebtoken'
 
 let cabinCollection: Collection
+let accountCollection: Collection
+
+const mockAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Eduardo',
+    email: 'email@mail.com',
+    password: '123',
+    role: 'admin'
+  })
+  const id = res.insertedId.toHexString()
+  const accessToken = jwt.sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne(
+    {
+      _id: res.insertedId
+    },
+    {
+      $set: {
+        accessToken
+      }
+    }
+  )
+  return accessToken
+}
 
 describe('Cabin', () => {
   beforeAll(async () => {
@@ -18,6 +43,8 @@ describe('Cabin', () => {
   beforeEach(async () => {
     cabinCollection = MongoHelper.getCollection('cabins')
     await cabinCollection.deleteMany({})
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
   test('should return 403 on add cabin', async () => {
     await request(app)
@@ -30,5 +57,20 @@ describe('Cabin', () => {
         description: 'A Cabin'
       })
       .expect(403)
+  })
+
+  test('should return 204 on add cabin with valid accessToken', async () => {
+    const accessToken = await mockAccessToken()
+    await request(app)
+      .post('/api/cabin')
+      .set('x-access-token', accessToken)
+      .send({
+        name: 'Cabin01',
+        maxCapacity: 4,
+        regularPrice: 30,
+        discount: 5,
+        description: 'A Cabin'
+      })
+      .expect(204)
   })
 })

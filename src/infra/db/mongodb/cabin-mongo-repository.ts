f67ca4ec9485773f6ba8dbe4278d87c071b7c9d2
Eduardo Lima <dlima78@hotmail.com>
2023-10-23 17:50:28 -0300

    feat: ensure CabinMongoRepository load all cabins on success

--- src/infra/db/mongodb/cabin-mongo-repository.ts
+++ src/infra/db/mongodb/cabin-mongo-repository.ts
@@ -1,9 +1,19 @@
-import { type AddCabinRepository } from '@/data/protocols'
+import {
+  type LoadCabinsRepository,
+  type AddCabinRepository
+} from '@/data/protocols'
 import { MongoHelper } from './mongo-helper'
 
-export class CabinMongoRepository implements AddCabinRepository {
+export class CabinMongoRepository
+implements AddCabinRepository, LoadCabinsRepository {
   async add (data: AddCabinRepository.Params): Promise<void> {
     const cabinCollection = MongoHelper.getCollection('cabins')
     await cabinCollection.insertOne(data)
+  }
+
+  async loadAll (): Promise<LoadCabinsRepository.Result> {
+    const cabinCollection = MongoHelper.getCollection('cabins')
+    const cabins = await cabinCollection.find({}).toArray()
+    return MongoHelper.mapCollection(cabins)
   }
 }

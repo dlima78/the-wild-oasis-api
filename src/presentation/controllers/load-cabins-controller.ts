b88f8cffaaf6a21f1f4bc53ef41d217f9ec069ae
Eduardo Lima <dlima78@hotmail.com>
2023-10-23 19:03:07 -0300

    feat: ensure LoadCabinController returns 204 if LoadCabins returns empty

--- src/presentation/controllers/load-cabins-controller.ts
+++ src/presentation/controllers/load-cabins-controller.ts
@@ -1,12 +1,15 @@
 import { type LoadCabins } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { ok } from '@/presentation/helpers'
+import { noContent, ok } from '@/presentation/helpers'
 
 export class LoadCabinsController implements Controller {
   constructor (private readonly loadCabins: LoadCabins) {}
 
   async handle (): Promise<HttpResponse> {
     const cabins = await this.loadCabins.load()
+    if (cabins.length === 0) {
+      return noContent()
+    }
     return ok(cabins)
   }
 }

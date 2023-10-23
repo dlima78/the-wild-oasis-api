4804caef0d054a43b9a262d37c497b4973ca0492
Eduardo Lima <dlima78@hotmail.com>
2023-10-23 18:54:31 -0300

    feat: ensure LoadCabinsController returns 200 on success

--- src/presentation/controllers/load-cabins-controller.ts
+++ src/presentation/controllers/load-cabins-controller.ts
@@ -1,14 +1,12 @@
 import { type LoadCabins } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
+import { ok } from '@/presentation/helpers'
 
 export class LoadCabinsController implements Controller {
   constructor (private readonly loadCabins: LoadCabins) {}
 
   async handle (): Promise<HttpResponse> {
-    await this.loadCabins.load()
-    return {
-      statusCode: 400,
-      body: ''
-    }
+    const cabins = await this.loadCabins.load()
+    return ok(cabins)
   }
 }

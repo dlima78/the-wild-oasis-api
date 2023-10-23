9b1f68c08ee1d66c6e2bf2783d0ef75d5ce4fced
Eduardo Lima <dlima78@hotmail.com>
2023-10-23 19:08:04 -0300

    feat: ensure LoadCabinsController returns 500 if LoadCabins throws

--- src/presentation/controllers/load-cabins-controller.ts
+++ src/presentation/controllers/load-cabins-controller.ts
@@ -1,15 +1,19 @@
 import { type LoadCabins } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { noContent, ok } from '@/presentation/helpers'
+import { noContent, ok, serverError } from '@/presentation/helpers'
 
 export class LoadCabinsController implements Controller {
   constructor (private readonly loadCabins: LoadCabins) {}
 
   async handle (): Promise<HttpResponse> {
-    const cabins = await this.loadCabins.load()
-    if (cabins.length === 0) {
-      return noContent()
+    try {
+      const cabins = await this.loadCabins.load()
+      if (cabins.length === 0) {
+        return noContent()
+      }
+      return ok(cabins)
+    } catch (error) {
+      return serverError(error as Error)
     }
-    return ok(cabins)
   }
 }

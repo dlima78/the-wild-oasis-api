b2bee9a433a153c7d9e322456fbe08f4bb6c7230
Eduardo Lima <dlima78@hotmail.com>
2023-10-24 15:53:44 -0300

    refactor: change SaveCabin usecase to receive id as optional

--- src/domain/usecases/save-cabin.ts
+++ src/domain/usecases/save-cabin.ts
@@ -1,9 +1,15 @@
-import { type CabinModel } from '@/domain/models'
-
 export interface SaveCabin {
   save: (data: SaveCabin.Params) => Promise<void>
 }
 
 export namespace SaveCabin {
-  export type Params = Omit<CabinModel, 'id'>
+  export type Params = {
+    id?: string
+    name: string
+    maxCapacity: number
+    regularPrice: number
+    discount: number
+    description: string
+    image?: string
+  }
 }

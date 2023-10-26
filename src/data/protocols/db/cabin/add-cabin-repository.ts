26486573ee28543e29b1f5ba192922334618d1f9
Eduardo Lima <dlima78@hotmail.com>
2023-10-26 17:10:43 -0300

    refactor: change AddCabinRepository return

--- src/data/protocols/db/cabin/add-cabin-repository.ts
+++ src/data/protocols/db/cabin/add-cabin-repository.ts
@@ -1,7 +1,7 @@
 import { type AddCabin } from '@/domain/usecases'
 
 export interface AddCabinRepository {
-  add: (data: AddCabinRepository.Params) => Promise<void>
+  add: (data: AddCabinRepository.Params) => Promise<boolean>
 }
 
 export namespace AddCabinRepository {

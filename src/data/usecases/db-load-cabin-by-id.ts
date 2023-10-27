3dbcc05cd882ca2500c770e0f566884f74b595b3
Eduardo Lima <dlima78@hotmail.com>
2023-10-27 16:30:51 -0300

    feat: ensure DbLoadCabinById returns a cabin on success

--- src/data/usecases/db-load-cabin-by-id.ts
+++ src/data/usecases/db-load-cabin-by-id.ts
@@ -7,15 +7,7 @@ export class DbLoadCabinById implements LoadCabinById {
   ) {}
 
   async loadById (id: string): Promise<LoadCabinById.Result> {
-    await this.loadCabinByIdRepository.loadById(id)
-    return {
-      id: '',
-      name: '',
-      maxCapacity: 0,
-      regularPrice: 0,
-      discount: 0,
-      description: '',
-      image: ''
-    }
+    const cabin = await this.loadCabinByIdRepository.loadById(id)
+    return cabin
   }
 }

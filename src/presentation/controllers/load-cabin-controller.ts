64d93bb62d146fc4d257d8779402219e86ab862b
Eduardo Lima <dlima78@hotmail.com>
2023-10-30 15:43:45 -0300

    feat: ensure LoadCabinController returns 500 if LoadCabin fails

--- src/presentation/controllers/load-cabin-controller.ts
+++ src/presentation/controllers/load-cabin-controller.ts
@@ -1,16 +1,20 @@
 import { type LoadCabin } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { noContent, ok } from '@/presentation/helpers'
+import { noContent, ok, serverError } from '@/presentation/helpers'
 
 export class LoadCabinController implements Controller {
   constructor (private readonly loadCabin: LoadCabin) {}
 
   async handle (request: LoadCabinController.Request): Promise<HttpResponse> {
-    const cabinModel = await this.loadCabin.loadById(request.id)
-    if (cabinModel) {
-      return ok(cabinModel)
+    try {
+      const cabinModel = await this.loadCabin.loadById(request.id)
+      if (cabinModel) {
+        return ok(cabinModel)
+      }
+      return noContent()
+    } catch (error) {
+      return serverError(error as Error)
     }
-    return noContent()
   }
 }
 

351e19ba67887405da1cb1fa9b57367d8f501a53
Eduardo Lima <dlima78@hotmail.com>
2023-10-30 15:26:36 -0300

    feat: ensure LoadCabinController retuns 200 on success

--- src/presentation/controllers/load-cabin-controller.ts
+++ src/presentation/controllers/load-cabin-controller.ts
@@ -1,11 +1,15 @@
 import { type LoadCabin } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
+import { ok } from '@/presentation/helpers'
 
 export class LoadCabinController implements Controller {
   constructor (private readonly loadCabin: LoadCabin) {}
 
   async handle (request: LoadCabinController.Request): Promise<HttpResponse> {
-    await this.loadCabin.loadById(request.id)
+    const cabinModel = await this.loadCabin.loadById(request.id)
+    if (cabinModel) {
+      return ok(cabinModel)
+    }
     return {
       statusCode: 0,
       body: ''

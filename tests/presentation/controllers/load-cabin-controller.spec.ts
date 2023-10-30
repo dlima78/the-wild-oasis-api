5e62261ac7e4f34764f6b404a511d48d1df3ae0b
Eduardo Lima <dlima78@hotmail.com>
2023-10-30 15:27:05 -0300

    test: ensure LoadCabinController retuns 200 on success

--- tests/presentation/controllers/load-cabin-controller.spec.ts
+++ tests/presentation/controllers/load-cabin-controller.spec.ts
@@ -1,6 +1,7 @@
 import { LoadCabinController } from '@/presentation/controllers'
 import { LoadCabinSpy } from '@/tests/presentation/mocks'
 import { faker } from '@faker-js/faker'
+import { ok } from '@/presentation/helpers'
 
 type SutTypes = {
   loadCabinSpy: LoadCabinSpy
@@ -26,5 +27,11 @@ describe('LoadCabin controller', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(loadCabinSpy.id).toEqual(request.id)
+  })
+
+  test('should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(ok(httpResponse.body))
   })
 })

249f4d6e8ad12a1be8a858df109ef2aada984b76
Eduardo Lima <dlima78@hotmail.com>
2023-10-31 19:40:37 -0300

    feat: ensure DbDeleteCabin returns true if DeleteCabinRepository succeeds

--- src/data/usecases/db-delete-cabin.ts
+++ src/data/usecases/db-delete-cabin.ts
@@ -4,7 +4,6 @@ import { type DeleteCabinRepository } from '@/data/protocols'
 export class DbDeleteCabin implements DeleteCabin {
   constructor (private readonly deleteCabinRepository: DeleteCabinRepository) {}
   async delete (cabinId: DeleteCabin.Param): Promise<boolean> {
-    await this.deleteCabinRepository.delete(cabinId)
-    return false
+    return await this.deleteCabinRepository.delete(cabinId)
   }
 }

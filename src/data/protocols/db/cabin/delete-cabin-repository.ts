2f78dde3ce48b505ed560ee8be19f3c87847e83d
Eduardo Lima <dlima78@hotmail.com>
2023-11-01 11:02:12 -0300

    fix: bug

--- src/data/protocols/db/cabin/delete-cabin-repository.ts
+++ src/data/protocols/db/cabin/delete-cabin-repository.ts
@@ -1,9 +1,3 @@
-import { type DeleteCabin } from '@/domain/usecases'
-
 export interface DeleteCabinRepository {
-  delete: (cabinId: DeleteCabinRepository.Param) => Promise<boolean>
-}
-
-export namespace DeleteCabinRepository {
-  export type Param = DeleteCabin.Param
+  delete: (cabinId: string) => Promise<boolean>
 }

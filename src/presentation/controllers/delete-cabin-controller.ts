13409e85ac334a2e7637d4452a262e8323c3db2c
Eduardo Lima <dlima78@hotmail.com>
2023-11-01 09:54:29 -0300

    feat: ensure DeleteCabinController returns 500 if DeleteCabin throws

--- src/presentation/controllers/delete-cabin-controller.ts
+++ src/presentation/controllers/delete-cabin-controller.ts
@@ -1,17 +1,21 @@
 import { type DeleteCabin } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { forbidden, noContent } from '@/presentation/helpers'
+import { forbidden, noContent, serverError } from '@/presentation/helpers'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class DeleteCabinController implements Controller {
   constructor (private readonly deleteCabin: DeleteCabin) {}
 
   async handle (request: DeleteCabinController.Request): Promise<HttpResponse> {
-    const isValid = await this.deleteCabin.delete(request.cabinId)
-    if (!isValid) {
-      return forbidden(new InvalidParamError('cabinId'))
+    try {
+      const isValid = await this.deleteCabin.delete(request.cabinId)
+      if (!isValid) {
+        return forbidden(new InvalidParamError('cabinId'))
+      }
+      return noContent()
+    } catch (error) {
+      return serverError(error as Error)
     }
-    return noContent()
   }
 }
 

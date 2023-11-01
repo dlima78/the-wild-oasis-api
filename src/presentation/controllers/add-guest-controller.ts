f0957054a23f373946f467cb190852f6d73fb03c
Eduardo Lima <dlima78@hotmail.com>
2023-11-01 15:53:40 -0300

    feat: ensure AddGuestController calls AddGuest  with correct values

--- src/presentation/controllers/add-guest-controller.ts
+++ src/presentation/controllers/add-guest-controller.ts
@@ -4,15 +4,20 @@ import {
   type Validation
 } from '@/presentation/protocols'
 import { badRequest } from '@/presentation/helpers'
+import { type AddGuest } from '@/domain/usecases'
 
 export class AddGuestController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addGuest: AddGuest
+  ) {}
 
   async handle (request: AddGuestController.Request): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
+    await this.addGuest.add(request)
     return {
       statusCode: 0,
       body: ''

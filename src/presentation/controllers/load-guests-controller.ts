4888b1e8a8ef7bf12742698a94104deaf8ec16ce
Eduardo Lima <dlima78@hotmail.com>
2023-11-02 10:10:21 -0300

    feat: ensure LoadGuestsController returns 204 if LoadGuests returns empty

--- src/presentation/controllers/load-guests-controller.ts
+++ src/presentation/controllers/load-guests-controller.ts
@@ -1,12 +1,15 @@
 import { type HttpResponse, type Controller } from '@/presentation/protocols'
 import { type LoadGuests } from '@/domain/usecases'
-import { ok } from '@/presentation/helpers'
+import { noContent, ok } from '@/presentation/helpers'
 
-export class LoadGuestController implements Controller {
+export class LoadGuestsController implements Controller {
   constructor (private readonly loadGuests: LoadGuests) {}
 
   async handle (): Promise<HttpResponse> {
     const guests = await this.loadGuests.load()
+    if (guests.length === 0) {
+      return noContent()
+    }
     return ok(guests)
   }
 }

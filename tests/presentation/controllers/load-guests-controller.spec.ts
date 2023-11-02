abb3063916477534378edd21d5c64505cf1774d3
Eduardo Lima <dlima78@hotmail.com>
2023-11-02 10:10:53 -0300

    test: ensure LoadGuestsController returns 204 if LoadGuests returns empty

--- tests/presentation/controllers/load-guests-controller.spec.ts
+++ tests/presentation/controllers/load-guests-controller.spec.ts
@@ -1,25 +1,32 @@
-import { LoadGuestController } from '@/presentation/controllers'
+import { LoadGuestsController } from '@/presentation/controllers'
 import { LoadGuestsSpy } from '@/tests/presentation/mocks'
-import { ok } from '@/presentation/helpers'
+import { noContent, ok } from '@/presentation/helpers'
 
 type SutTypes = {
   loadGuestsSpy: LoadGuestsSpy
-  sut: LoadGuestController
+  sut: LoadGuestsController
 }
 
 const makeSut = (): SutTypes => {
   const loadGuestsSpy = new LoadGuestsSpy()
-  const sut = new LoadGuestController(loadGuestsSpy)
+  const sut = new LoadGuestsController(loadGuestsSpy)
   return {
     sut,
     loadGuestsSpy
   }
 }
 
-describe('Add Guest Controller', () => {
+describe('Load Guests Controller', () => {
   test('should return 200 on success', async () => {
     const { sut, loadGuestsSpy } = makeSut()
     const httpResponse = await sut.handle()
     expect(httpResponse).toEqual(ok(loadGuestsSpy.result))
+  })
+
+  test('should return 204 if LoadGuests returns empty', async () => {
+    const { sut, loadGuestsSpy } = makeSut()
+    loadGuestsSpy.result = []
+    const httpResponse = await sut.handle()
+    expect(httpResponse).toEqual(noContent())
   })
 })

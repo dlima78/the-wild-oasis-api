0e2bf1ca05723cf00af9225e3aed5999291fb4ca
Eduardo Lima <dlima78@hotmail.com>
2023-11-02 10:45:52 -0300

    feat: ensure LoadGuestsController returns 500 if LoadGuests throws

--- src/presentation/controllers/load-guests-controller.ts
+++ src/presentation/controllers/load-guests-controller.ts
@@ -1,15 +1,19 @@
 import { type HttpResponse, type Controller } from '@/presentation/protocols'
 import { type LoadGuests } from '@/domain/usecases'
-import { noContent, ok } from '@/presentation/helpers'
+import { noContent, ok, serverError } from '@/presentation/helpers'
 
 export class LoadGuestsController implements Controller {
   constructor (private readonly loadGuests: LoadGuests) {}
 
   async handle (): Promise<HttpResponse> {
-    const guests = await this.loadGuests.load()
-    if (guests.length === 0) {
-      return noContent()
+    try {
+      const guests = await this.loadGuests.load()
+      if (guests.length === 0) {
+        return noContent()
+      }
+      return ok(guests)
+    } catch (error) {
+      return serverError(error as Error)
     }
-    return ok(guests)
   }
 }

a05ab2f7f5407ce16cbd2ea1ec3895095b460b5a
Eduardo Lima <dlima78@hotmail.com>
2023-11-01 21:27:58 -0300

    feat: ensure GeustMongoRepository load all guests on success

--- src/infra/db/mongodb/guest-mongo-repository.ts
+++ src/infra/db/mongodb/guest-mongo-repository.ts
@@ -1,10 +1,16 @@
-import { type AddGuestRepository } from '@/data/protocols'
+import { type LoadGuestsRepository, type AddGuestRepository } from '@/data/protocols'
 import { MongoHelper } from './mongo-helper'
 
-export class GuestMongoRepository implements AddGuestRepository {
+export class GuestMongoRepository implements AddGuestRepository, LoadGuestsRepository {
   async add (data: AddGuestRepository.Params): Promise<boolean> {
     const guestCollection = MongoHelper.getCollection('guests')
     const result = await guestCollection.insertOne(data)
     return result !== null
+  }
+
+  async loadAll (): Promise<LoadGuestsRepository.Result> {
+    const guestCollection = MongoHelper.getCollection('guests')
+    const guestsModel = await guestCollection.find({}).toArray()
+    return MongoHelper.mapCollection(guestsModel)
   }
 }

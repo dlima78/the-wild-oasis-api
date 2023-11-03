727194e45ca1607c79a11a68a767b2e33c06af25
Eduardo Lima <dlima78@hotmail.com>
2023-11-03 15:22:33 -0300

    feat: ensure DeleteGuestController returns 403 if UpdateGuest returns false

--- src/presentation/controllers/delete-guest-controller.ts
+++ src/presentation/controllers/delete-guest-controller.ts
@@ -1,11 +1,16 @@
 import { type DeleteGuest } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
+import { forbidden } from '../helpers'
+import { InvalidParamError } from '../errors'
 
 export class DeleteGuestController implements Controller {
   constructor (private readonly deleteGuest: DeleteGuest) {}
 
   async handle (request: DeleteGuestController.Request): Promise<HttpResponse> {
-    await this.deleteGuest.delete(request.guestId)
+    const isDeleted = await this.deleteGuest.delete(request.guestId)
+    if (!isDeleted) {
+      return forbidden(new InvalidParamError('guestId'))
+    }
     return {
       statusCode: 0,
       body: ''

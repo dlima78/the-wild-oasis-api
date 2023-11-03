08ad1a1cd895f6b9e9a2a222a8d074eef17fa33c
Eduardo Lima <dlima78@hotmail.com>
2023-11-03 15:25:37 -0300

    feat: ensure DeleteGuestController return 500 if DeleteGuest throws

--- src/presentation/controllers/delete-guest-controller.ts
+++ src/presentation/controllers/delete-guest-controller.ts
@@ -1,19 +1,23 @@
 import { type DeleteGuest } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { forbidden } from '../helpers'
-import { InvalidParamError } from '../errors'
+import { forbidden, serverError } from '@/presentation/helpers'
+import { InvalidParamError } from '@/presentation/errors'
 
 export class DeleteGuestController implements Controller {
   constructor (private readonly deleteGuest: DeleteGuest) {}
 
   async handle (request: DeleteGuestController.Request): Promise<HttpResponse> {
-    const isDeleted = await this.deleteGuest.delete(request.guestId)
-    if (!isDeleted) {
-      return forbidden(new InvalidParamError('guestId'))
-    }
-    return {
-      statusCode: 0,
-      body: ''
+    try {
+      const isDeleted = await this.deleteGuest.delete(request.guestId)
+      if (!isDeleted) {
+        return forbidden(new InvalidParamError('guestId'))
+      }
+      return {
+        statusCode: 0,
+        body: ''
+      }
+    } catch (error) {
+      return serverError(error as Error)
     }
   }
 }

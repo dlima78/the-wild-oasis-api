5e8ec0e19656b9d8f4bf2c121d139c1bc2f3d2d1
Eduardo Lima <dlima78@hotmail.com>
2023-11-03 10:40:18 -0300

    feat: ensure DbUpdateGuest returns a Guest on success

--- src/data/usecases/db-update-guest.ts
+++ src/data/usecases/db-update-guest.ts
@@ -1,7 +1,6 @@
 import { type UpdateGuestRepository } from '@/data/protocols'
 import { type GuestModel } from '@/domain/models'
 import { type UpdateGuest } from '@/domain/usecases'
-import { faker } from '@faker-js/faker'
 
 export class DbUpdateGuest implements UpdateGuest {
   constructor (
@@ -9,14 +8,6 @@ export class DbUpdateGuest implements UpdateGuest {
   ) {}
 
   async update (params: UpdateGuest.Params): Promise<GuestModel> {
-    await this.updateGuestRepository.update(params)
-    return {
-      id: faker.string.uuid(),
-      fullName: faker.person.fullName(),
-      email: faker.internet.email(),
-      nationality: faker.location.country(),
-      countryFlag: faker.lorem.word(3),
-      nationalId: faker.location.countryCode()
-    }
+    return await this.updateGuestRepository.update(params)
   }
 }

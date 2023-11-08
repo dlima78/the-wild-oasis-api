2d98bc07aa1ca7d1257061eab4949f4460a1fe67
Eduardo Lima <dlima78@hotmail.com>
2023-11-08 19:17:40 -0300

    feat: ensure AddBookingController calls AddBooking with correct values

--- src/presentation/controllers/add-booking-controller.ts
+++ src/presentation/controllers/add-booking-controller.ts
@@ -1,14 +1,19 @@
 import { type HttpResponse, type Controller, type Validation } from '@/presentation/protocols'
 import { badRequest } from '../helpers'
+import { type AddBooking } from '@/domain/usecases'
 
 export class AddBookingController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addBooking: AddBooking
+  ) {}
 
   async handle (request: AddBookingController.Request): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
+    await this.addBooking.add(request)
     return {
       statusCode: 0,
       body: 0

37c60c1597533987570c5bf3fb078987454710de
Eduardo Lima <dlima78@hotmail.com>
2023-11-10 16:26:24 -0300

    feat: ensure LoadBookingsController returns 204 if LoadBookings retuns no booking

--- src/presentation/controllers/load-bookings-controller.ts
+++ src/presentation/controllers/load-bookings-controller.ts
@@ -1,12 +1,15 @@
 import { type LoadBookings } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { ok } from '../helpers'
+import { noContent, ok } from '@/presentation/helpers'
 
 export class LoadBookingsController implements Controller {
   constructor (private readonly loadBookings: LoadBookings) {}
 
   async handle (): Promise<HttpResponse> {
     const bookings = await this.loadBookings.load()
+    if (bookings.length === 0) {
+      return noContent()
+    }
     return ok(bookings)
   }
 }

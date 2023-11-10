9740137ef08633b783643ba08a5aa201616127fa
Eduardo Lima <dlima78@hotmail.com>
2023-11-10 16:21:57 -0300

    feat: ensure LoadBookingsController returns 200 on success

--- src/presentation/controllers/load-bookings-controller.ts
+++ src/presentation/controllers/load-bookings-controller.ts
@@ -1,14 +1,12 @@
 import { type LoadBookings } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
+import { ok } from '../helpers'
 
 export class LoadBookingsController implements Controller {
   constructor (private readonly loadBookings: LoadBookings) {}
 
   async handle (): Promise<HttpResponse> {
-    await this.loadBookings.load()
-    return {
-      statusCode: 0,
-      body: ''
-    }
+    const bookings = await this.loadBookings.load()
+    return ok(bookings)
   }
 }

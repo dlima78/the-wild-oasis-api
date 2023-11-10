3e8f8fd3f6af8e0c4ca5ddb816e5433624491b06
Eduardo Lima <dlima78@hotmail.com>
2023-11-10 16:29:13 -0300

    feat: ensure LoadBookingsController returns 500 if LoadBookings throws

--- src/presentation/controllers/load-bookings-controller.ts
+++ src/presentation/controllers/load-bookings-controller.ts
@@ -1,15 +1,19 @@
 import { type LoadBookings } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { noContent, ok } from '@/presentation/helpers'
+import { noContent, ok, serverError } from '@/presentation/helpers'
 
 export class LoadBookingsController implements Controller {
   constructor (private readonly loadBookings: LoadBookings) {}
 
   async handle (): Promise<HttpResponse> {
-    const bookings = await this.loadBookings.load()
-    if (bookings.length === 0) {
-      return noContent()
+    try {
+      const bookings = await this.loadBookings.load()
+      if (bookings.length === 0) {
+        return noContent()
+      }
+      return ok(bookings)
+    } catch (error) {
+      return serverError(error as Error)
     }
-    return ok(bookings)
   }
 }

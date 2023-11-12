304ba78b3421e554e6d942d301b0e901a1e6fb28
Eduardo Lima <dlima78@hotmail.com>
2023-11-11 21:14:19 -0300

    feat: ensure DbUpdateBooking return UpdateResult on success

--- src/data/usecases/db-update-booking.ts
+++ src/data/usecases/db-update-booking.ts
@@ -1,11 +1,9 @@
 import { type UpdateBooking } from '@/domain/usecases'
 import { type UpdateBookingRepository } from '@/data/protocols'
-import { mockBookingModel } from '@/tests/domain/mocks'
 
 export class DbUpdateBooking implements UpdateBooking {
   constructor (private readonly updateBookingRepository: UpdateBookingRepository) {}
   async update (data: UpdateBooking.Params): Promise<UpdateBooking.Result> {
-    await this.updateBookingRepository.update(data)
-    return mockBookingModel()
+    return await this.updateBookingRepository.update(data)
   }
 }

2c5423281fb06ea0ae70c7cf78f2ccee4fe7c94c
Eduardo Lima <dlima78@hotmail.com>
2023-11-13 15:25:49 -0300

    feat: ensure DeleteBookingController returns 500 if DeleteBooking throws

--- src/presentation/controllers/delete-booking-controller.ts
+++ src/presentation/controllers/delete-booking-controller.ts
@@ -1,17 +1,21 @@
 import { type DeleteBooking } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { forbidden, noContent } from '@/presentation/helpers'
+import { forbidden, noContent, serverError } from '@/presentation/helpers'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class DeleteBookingController implements Controller {
   constructor (private readonly deleteBooking: DeleteBooking) {}
 
   async handle (request: DeleteBookingController.Request): Promise<HttpResponse> {
-    const isDeleted = await this.deleteBooking.delete(request.bookingId)
-    if (!isDeleted) {
-      return forbidden(new InvalidParamError('bookingId'))
+    try {
+      const isDeleted = await this.deleteBooking.delete(request.bookingId)
+      if (!isDeleted) {
+        return forbidden(new InvalidParamError('bookingId'))
+      }
+      return noContent()
+    } catch (error) {
+      return serverError(error as Error)
     }
-    return noContent()
   }
 }
 

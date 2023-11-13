d359adcff428f772f3295d5d5f3513fbfafeafd0
Eduardo Lima <dlima78@hotmail.com>
2023-11-13 15:20:16 -0300

    feat: ensure DeleteBookingController returns 403 if DeleteBooking returns false

--- src/presentation/controllers/delete-booking-controller.ts
+++ src/presentation/controllers/delete-booking-controller.ts
@@ -1,11 +1,16 @@
 import { type DeleteBooking } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
+import { forbidden } from '@/presentation/helpers'
+import { InvalidParamError } from '@/presentation/errors'
 
 export class DeleteBookingController implements Controller {
   constructor (private readonly deleteBooking: DeleteBooking) {}
 
   async handle (request: DeleteBookingController.Request): Promise<HttpResponse> {
-    await this.deleteBooking.delete(request.bookingId)
+    const isDeleted = await this.deleteBooking.delete(request.bookingId)
+    if (!isDeleted) {
+      return forbidden(new InvalidParamError('bookingId'))
+    }
     return {
       statusCode: 0,
       body: ''

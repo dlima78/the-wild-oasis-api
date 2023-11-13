cc4880e4a11db33fde9e72a9d3315b4c5bcea74f
Eduardo Lima <dlima78@hotmail.com>
2023-11-13 11:45:12 -0300

    feat: ensure UpdateBookingController retuns 500 if UpdateBooking throws

--- src/presentation/controllers/update-booking-controller.ts
+++ src/presentation/controllers/update-booking-controller.ts
@@ -1,13 +1,17 @@
 import { type UpdateBooking } from '@/domain/usecases'
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { ok } from '../helpers'
+import { ok, serverError } from '../helpers'
 
 export class UpdateBookingController implements Controller {
   constructor (private readonly updateBooking: UpdateBooking) {}
 
   async handle (request: UpdateBookingController.Request): Promise<HttpResponse> {
-    const updateBooking = await this.updateBooking.update(request)
-    return ok(updateBooking)
+    try {
+      const updateBooking = await this.updateBooking.update(request)
+      return ok(updateBooking)
+    } catch (error) {
+      return serverError(error as Error)
+    }
   }
 }
 
